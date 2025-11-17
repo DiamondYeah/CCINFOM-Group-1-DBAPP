49af7ef74fede5d70b0add150ac0fa135edf8a03
jhiroliri0 <jhirolirio@gmail.com>
2025-11-17 18:17:29 +0800

    Refactor Book class to use Date and capacity fields

--- Java_Files/Book.java
+++ Java_Files/Book.java
@@ -1,49 +1,37 @@
+import java.sql.Date;
+
 public class Book {
     private int bookingID;
     private int organizerID;
     private int locationID;
-    private int pax;
-    private String startDate;
-    private String endDate;
-    private String bookDate;
+    private int currentCapacity;
+    private int maxCapacity;
+    private Date startDate;
+    private Date endDate;
+    private String status;
 
-    public Book(int bookingID, int organizerID, int locationID, int pax, String startDate, String endDate){
-         this.bookingID = bookingID;
-         this.organizerID = organizerID;
-         this.pax = pax;
-         this.startDate = startDate;
-         this.endDate = endDate;
+    public Book(int bookingID, int organizerID, int locationID, int currentCapacity, int maxCapacity, Date startDate, Date endDate, String status) {
+        this.bookingID = bookingID;
+        this.organizerID = organizerID;
+        this.locationID = locationID;
+        this.currentCapacity = currentCapacity;
+        this.maxCapacity = maxCapacity;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.status = status;
     }
 
-    public int getBookingID(){
-        return bookingID;
-    }
+    // Getters and setters
+    public int getBookingID() { return bookingID; }
+    public int getOrganizerID() { return organizerID; }
+    public int getLocationID() { return locationID; }
+    public int getCurrentCapacity() { return currentCapacity; }
+    public int getMaxCapacity() { return maxCapacity; }
+    public Date getStartDate() { return startDate; }
+    public Date getEndDate() { return endDate; }
+    public String getStatus() { return status; }
 
-    public int getOrganizerID(){
-        return organizerID;
-    }
-
-    public int getLocationID(){
-        return locationID;
-    }
-
-    public int getPax(){
-        return pax;
-    }
-
-    public String getStartDate(){
-        return startDate;
-    }
-
-    public String getEndDate(){
-        return endDate;
-    }
-
-    public String getBookDate(){
-        bookDate = startDate + " until " + endDate;
-        return bookDate;
-    }
-    
+    public void setCurrentCapacity(int currentCapacity) { this.currentCapacity = currentCapacity; }
+    public void setStartDate(Date startDate) { this.startDate = startDate; }
+    public void setEndDate(Date endDate) { this.endDate = endDate; }
 }
-
-
